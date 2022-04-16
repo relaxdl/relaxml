3e63e3fa36f78bf6ce1b92e8cc1f9ddd801ca3cc
relaxdl <13524563817@qq.com>
2022-04-16 11:43:45 +0800

    增加了fashion mnist datasets

---
+++ pytorch/datasets/fashion_mnist.py
@@ -0,0 +1,48 @@
+from typing import Tuple
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+
+def load_data_fashion_mnist(
+        batch_size: int,
+        resize: int = None,
+        root: str = '../data') -> Tuple[DataLoader, DataLoader]:
+    """
+    下载Fashion-MNIST数据集, 然后将其加载到内存中
+
+    1. 60000张训练图像和对应Label
+    2. 10000张测试图像和对应Label
+    3. 10个类别
+    4. 每张图像28x28x1的分辨率
+
+    >>> train_iter, test_iter = load_data_fashion_mnist(batch_size=256)
+    >>> for x, y in train_iter:
+    >>>     assert x.shape == (256, 1, 28, 28)
+    >>>     assert y.shape == (256, )
+    >>>     break
+    """
+    # 通过ToTensor实例将图像数据从PIL类型变换成32位浮点数格式
+    # 并除以255使得所有像素的数值均在0到1之间
+    trans = [transforms.ToTensor()]
+    if resize:
+        trans.insert(0, transforms.Resize(resize))
+    trans = transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(root=root,
+                                                    train=True,
+                                                    transform=trans,
+                                                    download=True)
+    mnist_test = torchvision.datasets.FashionMNIST(root=root,
+                                                   train=False,
+                                                   transform=trans,
+                                                   download=True)
+    return (DataLoader(mnist_train, batch_size, shuffle=True),
+            DataLoader(mnist_test, batch_size, shuffle=False))
+
+
+if __name__ == '__main__':
+    train_iter, test_iter = load_data_fashion_mnist(batch_size=256)
+    for x, y in train_iter:
+        assert x.shape == (256, 1, 28, 28)
+        assert y.shape == (256, )
+        break
