d3ffbe6abd2351397229c421381a7c4bcadef753
relaxdl <13524563817@qq.com>
2022-04-17 15:50:56 +0800

    重构了目录结构

---
+++ relaxml/pytorch/util.py
@@ -0,0 +1,131 @@
+import sys
+import time
+from typing import Tuple, List
+import torch
+from torch import Tensor
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.optim.optimizer import Optimizer
+import torchvision
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+
+
+def accuracy(y_hat: Tensor, y: Tensor) -> Tensor:
+    """
+    计算预测正确的数量
+
+    参数:
+    y_hat.shape: [batch_size, num_classes]
+    y.shape: [batch_size,]
+    """
+    _, predicted = torch.max(y_hat, 1)
+    cmp = predicted.type(y.dtype) == y
+    return cmp.type(y.dtype).sum()
+
+
+def train_gpu(net: nn.Module,
+              train_iter: DataLoader,
+              test_iter: DataLoader,
+              num_epochs: int = 10,
+              loss: nn.Module = None,
+              optimizer: Optimizer = None,
+              device: torch.device = None,
+              verbose: bool = False,
+              save_path: str = None) -> List[List[Tuple[int, float]]]:
+    """
+    用GPU训练模型
+    """
+    if device is None:
+        device = torch.device(
+            'cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    print('training on', device)
+    net.to(device)
+    if loss is None:
+        loss = nn.CrossEntropyLoss(reduction='mean')
+    if optimizer is None:
+        optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
+    times = []
+    history = [[], [], []]  # 记录: 训练集损失, 训练集准确率, 测试集准确率, 方便后续绘图
+    num_batches = len(train_iter)
+    best_test_acc = 0.0
+    for epoch in range(num_epochs):
+        # 训练
+        metric_train = [0.0] * 3  # 统计: 训练集损失之和, 训练集准确数量之和, 训练集样本数量之和
+        net.train()
+        train_iter_tqdm = tqdm(train_iter, file=sys.stdout)
+        for i, (X, y) in enumerate(train_iter_tqdm):
+            t_start = time.time()
+            optimizer.zero_grad()
+            X, y = X.to(device), y.to(device)
+            y_hat = net(X)
+            l = loss(y_hat, y)
+            l.backward()
+            optimizer.step()
+            with torch.no_grad():
+                metric_train[0] += float(l * X.shape[0])
+                metric_train[1] += float(accuracy(y_hat, y))
+                metric_train[2] += float(X.shape[0])
+            times.append(time.time() - t_start)
+            train_loss = metric_train[0] / metric_train[2]
+            train_acc = metric_train[1] / metric_train[2]
+            if (i + 1) % (num_batches // 5) == 0 or i == num_batches - 1:
+                history[0].append((epoch + (i + 1) / num_batches, train_loss))
+                history[1].append((epoch + (i + 1) / num_batches, train_acc))
+            train_iter_tqdm.desc = f'epoch {epoch}, step {i+1}, train loss {train_loss:.3f}, train acc {train_acc:.3f}'
+
+        # 评估
+        metric_test = [0.0] * 2  # 测试准确数量之和, 测试样本数量之和
+        net.eval()
+        with torch.no_grad():
+            for X, y in test_iter:
+                X, y = X.to(device), y.to(device)
+                metric_test[0] += float(accuracy(net(X), y))
+                metric_test[1] += float(X.shape[0])
+            test_acc = metric_test[0] / metric_test[1]
+            history[2].append((epoch + 1, test_acc))
+            print(f'epoch {epoch}, step {i+1}, train loss {train_loss:.3f}, '
+                  f'train acc {train_acc:.3f}, test acc {test_acc:.3f}')
+            if test_acc > best_test_acc and save_path:
+                best_test_acc = test_acc
+                torch.save(net.state_dict(), save_path)
+
+    print(f'train loss {train_loss:.3f}, train acc {train_acc:.3f}, '
+          f'test acc {test_acc:.3f}')
+    print(f'{metric_train[2] * num_epochs / sum(times):.1f} '
+          f'examples/sec on {str(device)}')
+    return history
+
+
+def plot_history(
+    history: List[List[Tuple[int, float]]], figsize: Tuple[int, int] = (6, 4)
+) -> None:
+    plt.figure(figsize=figsize)
+    # 训练集损失, 训练集准确率, 测试集准确率
+    num_epochs = len(history[2])
+    plt.plot(*zip(*history[0]), '-', label='train loss')
+    plt.plot(*zip(*history[1]), 'm--', label='train acc')
+    plt.plot(*zip(*history[2]), 'g-.', label='test acc')
+    plt.xlabel('epoch')
+    # 从epoch=1开始显示, 0-1这个范围的数据丢弃不展示,
+    # 因为只有训练完成1个epochs之后, 才会有第一条test acc记录
+    plt.xlim((1, num_epochs))
+    plt.grid()
+    plt.legend()
+    plt.show()
+
+
+def val_classify(num_classes, train_iter, test_iter):
+    net = torchvision.models.resnet18(pretrained=True)
+    # 替换model.fc:
+    # (fc): Linear(in_features=512, out_features=1000, bias=True)
+    net.fn = torch.nn.Linear(net.fc.in_features, num_classes)
+    kwargs = {
+        'num_epochs': 10,
+        'loss': nn.CrossEntropyLoss(reduction='mean'),
+        'optimizer': torch.optim.SGD(net.parameters(), lr=0.01),
+        'save_path': '../data/resnet18_val_classify.pth'
+    }
+    history = train_gpu(net, train_iter, test_iter, **kwargs)
+    plot_history(history)
