b3ee19a77d98651b243c07e75f7f709128fc1d6f
relaxdl <13524563817@qq.com>
2022-04-19 19:48:11 +0800

    update

---
+++ relaxml/pytorch/optimization/batch_norml.py
@@ -0,0 +1,290 @@
+from typing import Tuple, Union, List
+import sys
+import time
+import torch
+from torch import Tensor
+from torch import nn
+from torch.nn import functional as F
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torch.optim.optimizer import Optimizer
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+"""
+Batch Norm
+
+说明:
+https://tech.foxrelax.com/optimization/batch_norm/
+"""
+
+
+def load_data_fashion_mnist(
+        batch_size: int,
+        resize: Union[int, Tuple[int, int]] = None,
+        root: str = '../data') -> Tuple[DataLoader, DataLoader]:
+    """
+    下载Fashion-MNIST数据集, 然后将其加载到内存中
+
+    1. 60000张训练图像和对应Label
+    2. 10000张测试图像和对应Label
+    3. 10个类别
+    4. 每张图像28x28x1的分辨率
+
+    >>> train_iter, test_iter = load_data_fashion_mnist(batch_size=256)
+    >>> for x, y in train_iter:
+    >>>     assert x.shape == (256, 1, 28, 28)
+    >>>     assert y.shape == (256, )
+    >>>     break
+    """
+    # 通过ToTensor实例将图像数据从PIL类型变换成32位浮点数格式
+    # 并除以255使得所有像素的数值均在0到1之间
+    trans = [transforms.ToTensor()]
+    if resize:
+        trans.insert(0, transforms.Resize(resize))
+    trans = transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(root=root,
+                                                    train=True,
+                                                    transform=trans,
+                                                    download=True)
+    mnist_test = torchvision.datasets.FashionMNIST(root=root,
+                                                   train=False,
+                                                   transform=trans,
+                                                   download=True)
+    return (DataLoader(mnist_train, batch_size, shuffle=True),
+            DataLoader(mnist_test, batch_size, shuffle=False))
+
+
+def batch_norm(X, gamma, beta, moving_mean, moving_var, eps, momentum):
+    # 通过 `is_grad_enabled` 来判断当前模式是训练模式还是预测模式
+    if not torch.is_grad_enabled():
+        # 如果是在预测模式下, 直接使用传入的移动平均所得的均值和方差
+        X_hat = (X - moving_mean) / torch.sqrt(moving_var + eps)
+    else:
+        assert len(X.shape) in (2, 4)
+        if len(X.shape) == 2:
+            # 使用全连接层的情况, 计算特征维上的均值和方差
+            mean = X.mean(dim=0)
+            var = ((X - mean)**2).mean(dim=0)
+        else:
+            # 使用二维卷积层的情况, 计算通道维上(axis=1)的均值和方差
+            # 这里我们需要保持X的形状以便后面可以做广播运算
+            # 训练模式下, 用当前的均值和方差做标准化
+            mean = X.mean(dim=(0, 2, 3), keepdim=True)  # 形状: [1, `C`, 1, 1]
+            var = ((X - mean)**2).mean(dim=(0, 2, 3),
+                                       keepdim=True)  # 形状: [1, `C`, 1, 1]
+        # 训练模式下，用当前的均值和方差做标准化
+        X_hat = (X - mean) / torch.sqrt(var + eps)
+        # 更新移动平均的均值和方差
+        moving_mean = momentum * moving_mean + (1.0 - momentum) * mean
+        moving_var = momentum * moving_var + (1.0 - momentum) * var
+    Y = gamma * X_hat + beta  # 缩放和移位
+    return Y, moving_mean.data, moving_var.data
+
+
+class BatchNorm(nn.Module):
+    # `num_features`: 完全连接层的输出数量或卷积层的输出通道数
+    # `num_dims`: 2表示完全连接层, 4表示卷积层
+    def __init__(self, num_features, num_dims):
+        super().__init__()
+        if num_dims == 2:
+            shape = (1, num_features)
+        else:
+            shape = (1, num_features, 1, 1)
+        # 参与求梯度和迭代的拉伸和偏移参数, 分别初始化成1和0
+        self.gamma = nn.Parameter(torch.ones(shape))
+        self.beta = nn.Parameter(torch.zeros(shape))
+        # 非模型参数的变量初始化为0和1
+        self.moving_mean = torch.zeros(shape)
+        self.moving_var = torch.ones(shape)
+
+    def forward(self, X):
+        # 如果 `X` 不在内存上, 将 `moving_mean` 和 `moving_var`
+        # 复制到 `X` 所在显存上
+        if self.moving_mean.device != X.device:
+            self.moving_mean = self.moving_mean.to(X.device)
+            self.moving_var = self.moving_var.to(X.device)
+        # 保存更新过的 `moving_mean` 和 `moving_var`
+        Y, self.moving_mean, self.moving_var = batch_norm(X,
+                                                          self.gamma,
+                                                          self.beta,
+                                                          self.moving_mean,
+                                                          self.moving_var,
+                                                          eps=1e-5,
+                                                          momentum=0.9)
+        return Y
+
+
+def accuracy(y_hat: Tensor, y: Tensor) -> Tensor:
+    """
+    计算预测正确的数量
+
+    参数:
+    y_hat.shape: [batch_size, num_classes]
+    y.shape: [batch_size,]
+    """
+    _, predicted = torch.max(y_hat, 1)
+    cmp = predicted.type(y.dtype) == y
+    return cmp.type(y.dtype).sum()
+
+
+def train_gpu(net: nn.Module,
+              train_iter: DataLoader,
+              test_iter: DataLoader,
+              num_epochs: int = 10,
+              loss: nn.Module = None,
+              optimizer: Optimizer = None,
+              device: torch.device = None,
+              verbose: bool = False,
+              save_path: str = None) -> List[List[Tuple[int, float]]]:
+    """
+    用GPU训练模型
+    """
+    if device is None:
+        device = torch.device(
+            'cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    print('training on', device)
+    net.to(device)
+    if loss is None:
+        loss = nn.CrossEntropyLoss(reduction='mean')
+    if optimizer is None:
+        optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
+    times = []
+    history = [[], [], []]  # 记录: 训练集损失, 训练集准确率, 测试集准确率, 方便后续绘图
+    num_batches = len(train_iter)
+    best_test_acc = 0.0
+    for epoch in range(num_epochs):
+        # 训练
+        metric_train = [0.0] * 3  # 统计: 训练集损失之和, 训练集准确数量之和, 训练集样本数量之和
+        net.train()
+        train_iter_tqdm = tqdm(train_iter, file=sys.stdout)
+        for i, (X, y) in enumerate(train_iter_tqdm):
+            t_start = time.time()
+            optimizer.zero_grad()
+            X, y = X.to(device), y.to(device)
+            y_hat = net(X)
+            l = loss(y_hat, y)
+            l.backward()
+            optimizer.step()
+            with torch.no_grad():
+                metric_train[0] += float(l * X.shape[0])
+                metric_train[1] += float(accuracy(y_hat, y))
+                metric_train[2] += float(X.shape[0])
+            times.append(time.time() - t_start)
+            train_loss = metric_train[0] / metric_train[2]
+            train_acc = metric_train[1] / metric_train[2]
+            if (i + 1) % (num_batches // 5) == 0 or i == num_batches - 1:
+                history[0].append((epoch + (i + 1) / num_batches, train_loss))
+                history[1].append((epoch + (i + 1) / num_batches, train_acc))
+            train_iter_tqdm.desc = f'epoch {epoch}, step {i+1}, train loss {train_loss:.3f}, train acc {train_acc:.3f}'
+
+        # 评估
+        metric_test = [0.0] * 2  # 测试准确数量之和, 测试样本数量之和
+        net.eval()
+        with torch.no_grad():
+            for X, y in test_iter:
+                X, y = X.to(device), y.to(device)
+                metric_test[0] += float(accuracy(net(X), y))
+                metric_test[1] += float(X.shape[0])
+            test_acc = metric_test[0] / metric_test[1]
+            history[2].append((epoch + 1, test_acc))
+            print(f'epoch {epoch}, step {i+1}, train loss {train_loss:.3f}, '
+                  f'train acc {train_acc:.3f}, test acc {test_acc:.3f}')
+            if test_acc > best_test_acc and save_path:
+                best_test_acc = test_acc
+                torch.save(net.state_dict(), save_path)
+
+    print(f'train loss {train_loss:.3f}, train acc {train_acc:.3f}, '
+          f'test acc {test_acc:.3f}')
+    print(f'{metric_train[2] * num_epochs / sum(times):.1f} '
+          f'examples/sec on {str(device)}')
+    return history
+
+
+def plot_history(
+    history: List[List[Tuple[int, float]]], figsize: Tuple[int, int] = (6, 4)
+) -> None:
+    plt.figure(figsize=figsize)
+    # 训练集损失, 训练集准确率, 测试集准确率
+    num_epochs = len(history[2])
+    plt.plot(*zip(*history[0]), '-', label='train loss')
+    plt.plot(*zip(*history[1]), 'm--', label='train acc')
+    plt.plot(*zip(*history[2]), 'g-.', label='test acc')
+    plt.xlabel('epoch')
+    # 从epoch=1开始显示, 0-1这个范围的数据丢弃不展示,
+    # 因为只有训练完成1个epochs之后, 才会有第一条test acc记录
+    plt.xlim((1, num_epochs))
+    plt.grid()
+    plt.legend()
+    plt.show()
+
+
+def demo1():
+
+    class Reshape(nn.Module):
+
+        def forward(self, X):
+            return X.view(-1, 1, 28, 28)
+
+    net = nn.Sequential(
+        Reshape(),
+        nn.Conv2d(1, 6, kernel_size=5, padding=2),
+        BatchNorm(6, num_dims=4),
+        nn.Sigmoid(),
+        nn.AvgPool2d(kernel_size=2, stride=2),
+        nn.Conv2d(6, 16, kernel_size=5),
+        BatchNorm(16, num_dims=4),
+        nn.Sigmoid(),
+        nn.AvgPool2d(kernel_size=2, stride=2),
+        nn.Flatten(),
+        nn.Linear(16 * 5 * 5, 120),
+        BatchNorm(120, num_dims=2),
+        nn.Sigmoid(),
+        nn.Linear(120, 84),
+        BatchNorm(84, num_dims=2),
+        nn.Sigmoid(),
+        nn.Linear(84, 10),
+    )
+
+    batch_size, num_epochs = 256, 1.0, 10
+    loss = nn.CrossEntropyLoss()
+    train_iter, test_iter = load_data_fashion_mnist(batch_size=batch_size)
+    train_gpu(net, train_iter, test_iter, num_epochs, loss)
+
+
+def demo2():
+
+    class Reshape(nn.Module):
+
+        def forward(self, X):
+            return X.view(-1, 1, 28, 28)
+
+    net = nn.Sequential(
+        Reshape(),
+        nn.Conv2d(1, 6, kernel_size=5, padding=2),
+        nn.BatchNorm2d(6),
+        nn.Sigmoid(),
+        nn.AvgPool2d(kernel_size=2, stride=2),
+        nn.Conv2d(6, 16, kernel_size=5),
+        nn.BatchNorm2d(16),
+        nn.Sigmoid(),
+        nn.AvgPool2d(kernel_size=2, stride=2),
+        nn.Flatten(),
+        nn.Linear(16 * 5 * 5, 120),
+        nn.BatchNorm1d(120),
+        nn.Sigmoid(),
+        nn.Linear(120, 84),
+        nn.BatchNorm1d(84),
+        nn.Sigmoid(),
+        nn.Linear(84, 10),
+    )
+
+    batch_size, num_epochs = 256, 1.0, 10
+    loss = nn.CrossEntropyLoss()
+    train_iter, test_iter = load_data_fashion_mnist(batch_size=batch_size)
+    train_gpu(net, train_iter, test_iter, num_epochs, loss)
+
+
+if __name__ == '__main__':
+    demo2()
