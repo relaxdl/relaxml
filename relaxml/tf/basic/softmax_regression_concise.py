3a56cb422c9a3ebf12691d08cfc992371e9a77c9
relaxdl <13524563817@qq.com>
2022-05-05 16:23:15 +0800

    增加了softmax回归简洁实现 TF版

--- relaxml/tf/basic/softmax_regression_concise.py
+++ relaxml/tf/basic/softmax_regression_concise.py
@@ -4,10 +4,10 @@ import tensorflow as tf
 from matplotlib import pyplot as plt
 from tqdm import tqdm
 """
-softmax回归从零实现
+softmax回归简洁实现
 
 说明:
-https://tech.foxrelax.com/basic/softmax_regression_scratch/
+https://tech.foxrelax.com/basic/softmax_regression_concise/
 """
 
 
@@ -45,54 +45,17 @@ def load_data_fashion_mnist(batch_size, resize=None):
     return train_iter, test_iter
 
 
-def softmax(X):
-    """
-    >>> X = tf.random.normal((2, 5))
-    >>> X_prob = softmax(X)
-    >>> print(X_prob)
-        tf.Tensor([[0.14849083 0.14573483 0.3269037  0.1714058  0.20746487]
-                   [0.0757804  0.18379888 0.46541107 0.15378769 0.12122207]], 
-                  shape=(2, 5), dtype=float32)
-    >>> print(tf.reduce_sum(X_prob, 1))
-        tf.Tensor([1. 1.], shape=(2,), dtype=float32)
-    """
-    X_exp = tf.exp(X)
-    partition = tf.reduce_sum(X_exp, 1, keepdims=True)
-    return X_exp / partition  # 这里应用了广播机制
+def net():
+    net = tf.keras.models.Sequential()
+    net.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+    weight_initializer = tf.keras.initializers.RandomNormal(mean=0.0,
+                                                            stddev=0.01)
+    net.add(tf.keras.layers.Dense(10, kernel_initializer=weight_initializer))
+    return net
 
 
-def gen_params(num_inputs=784, num_outputs=10):
-    """
-    生成模型参数: [w, b]
-    """
-    W = tf.Variable(
-        tf.random.normal(shape=(num_inputs, num_outputs), mean=0, stddev=0.01))
-    b = tf.Variable(tf.zeros(num_outputs))
-    return W, b
-
-
-def net(X, W, b):
-    return softmax(tf.matmul(tf.reshape(X, (-1, W.shape[0])), W) + b)
-
-
-def cross_entropy(y_hat, y):
-    """
-    >>> y = tf.constant([0, 2])
-    >>> y_hat = tf.constant([[0.1, 0.3, 0.6], [0.3, 0.2, 0.5]])
-    >>> l = cross_entropy(y_hat, y)
-    >>> print(l)
-        tf.Tensor([2.3025851 0.6931472], shape=(2,), dtype=float32)
-    """
-    return -tf.math.log(
-        tf.boolean_mask(y_hat, tf.one_hot(y, depth=y_hat.shape[-1])))
-
-
-def sgd(params, grads, lr, batch_size):
-    """
-    小批量随机梯度下降
-    """
-    for param, grad in zip(params, grads):
-        param.assign_sub(lr * grad / batch_size)
+loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+trainer = tf.keras.optimizers.SGD(learning_rate=.1)
 
 
 def accuracy(y_hat, y):
@@ -108,9 +71,7 @@ def accuracy(y_hat, y):
     return float(tf.reduce_sum(tf.cast(cmp, y.dtype)))
 
 
-def train(net, train_iter, test_iter, num_epochs=10, lr=0.1):
-    W, b = gen_params()
-    loss = cross_entropy
+def train(net, train_iter, test_iter, num_epochs, loss, optimizer):
     times = []
     history = [[], [], []]  # 记录: 训练集损失, 训练集准确率, 测试集准确率, 方便后续绘图
     num_batches = len(train_iter)
@@ -122,12 +83,13 @@ def train(net, train_iter, test_iter, num_epochs=10, lr=0.1):
         for i, (X, y) in enumerate(train_iter_tqdm):
             t_start = time.time()
             with tf.GradientTape() as tape:
-                y_hat = net(X, W, b)
-                l = loss(y_hat, y)
-            grads = tape.gradient(l, [W, b])
-            sgd([W, b], grads, lr, X.shape[0])
+                y_hat = net(X)
+                l = loss(y, y_hat)
+            params = net.trainable_variables
+            grads = tape.gradient(l, params)
+            optimizer.apply_gradients(zip(grads, params))
 
-            metric_train[0] += float(tf.reduce_sum(l))
+            metric_train[0] += float(l * X.shape[0])
             metric_train[1] += float(accuracy(y_hat, y))
             metric_train[2] += float(X.shape[0])
             times.append(time.time() - t_start)
@@ -141,7 +103,7 @@ def train(net, train_iter, test_iter, num_epochs=10, lr=0.1):
         # 评估
         metric_test = [0.0] * 2  # 测试准确数量之和, 测试样本数量之和
         for X, y in test_iter:
-            metric_test[0] += float(accuracy(net(X, W, b), y))
+            metric_test[0] += float(accuracy(net(X), y))
             metric_test[1] += float(X.shape[0])
         test_acc = metric_test[0] / metric_test[1]
         history[2].append((epoch + 1, test_acc))
@@ -174,31 +136,38 @@ def plot_history(history, figsize=(6, 4)) -> None:
 
 def run() -> None:
     train_iter, test_iter = load_data_fashion_mnist(batch_size=256)
-    history = train(net, train_iter, test_iter, num_epochs=10, lr=0.1)
+    _net = net()
+    kwargs = {
+        'num_epochs': 10,
+        'loss':
+        tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        'optimizer': tf.keras.optimizers.SGD(learning_rate=0.1)
+    }
+    history = train(_net, train_iter, test_iter, **kwargs)
     plot_history(history)
 
 
 if __name__ == '__main__':
     run()
-# epoch 0, step 235, train loss 0.785, train acc 0.749: 100%|██████████| 235/235 [00:02<00:00, 97.32it/s]
-# epoch 0, step 235, train loss 0.785, train acc 0.749, test acc 0.781
-# epoch 1, step 235, train loss 0.571, train acc 0.813: 100%|██████████| 235/235 [00:02<00:00, 97.47it/s]
-# epoch 1, step 235, train loss 0.571, train acc 0.813, test acc 0.810
-# epoch 2, step 235, train loss 0.526, train acc 0.827: 100%|██████████| 235/235 [00:01<00:00, 122.93it/s]
-# epoch 2, step 235, train loss 0.526, train acc 0.827, test acc 0.816
-# epoch 3, step 235, train loss 0.501, train acc 0.832: 100%|██████████| 235/235 [00:01<00:00, 125.40it/s]
-# epoch 3, step 235, train loss 0.501, train acc 0.832, test acc 0.824
-# epoch 4, step 235, train loss 0.486, train acc 0.836: 100%|██████████| 235/235 [00:01<00:00, 124.46it/s]
-# epoch 4, step 235, train loss 0.486, train acc 0.836, test acc 0.826
-# epoch 5, step 235, train loss 0.474, train acc 0.840: 100%|██████████| 235/235 [00:01<00:00, 126.14it/s]
-# epoch 5, step 235, train loss 0.474, train acc 0.840, test acc 0.829
-# epoch 6, step 235, train loss 0.465, train acc 0.843: 100%|██████████| 235/235 [00:01<00:00, 124.39it/s]
-# epoch 6, step 235, train loss 0.465, train acc 0.843, test acc 0.829
-# epoch 7, step 235, train loss 0.458, train acc 0.845: 100%|██████████| 235/235 [00:01<00:00, 123.43it/s]
-# epoch 7, step 235, train loss 0.458, train acc 0.845, test acc 0.827
-# epoch 8, step 235, train loss 0.452, train acc 0.847: 100%|██████████| 235/235 [00:01<00:00, 121.72it/s]
-# epoch 8, step 235, train loss 0.452, train acc 0.847, test acc 0.833
-# epoch 9, step 235, train loss 0.447, train acc 0.847: 100%|██████████| 235/235 [00:01<00:00, 126.38it/s]
-# epoch 9, step 235, train loss 0.447, train acc 0.847, test acc 0.835
-# train loss 0.447, train acc 0.847, test acc 0.835
-# 35573.3 examples/sec
+# epoch 0, step 235, train loss 0.786, train acc 0.748: 100%|██████████| 235/235 [00:02<00:00, 115.39it/s]
+# epoch 0, step 235, train loss 0.786, train acc 0.748, test acc 0.791
+# epoch 1, step 235, train loss 0.571, train acc 0.813: 100%|██████████| 235/235 [00:01<00:00, 131.90it/s]
+# epoch 1, step 235, train loss 0.571, train acc 0.813, test acc 0.808
+# epoch 2, step 235, train loss 0.525, train acc 0.827: 100%|██████████| 235/235 [00:01<00:00, 148.33it/s]
+# epoch 2, step 235, train loss 0.525, train acc 0.827, test acc 0.819
+# epoch 3, step 235, train loss 0.500, train acc 0.832: 100%|██████████| 235/235 [00:01<00:00, 156.15it/s]
+# epoch 3, step 235, train loss 0.500, train acc 0.832, test acc 0.824
+# epoch 4, step 235, train loss 0.485, train acc 0.836: 100%|██████████| 235/235 [00:01<00:00, 155.82it/s]
+# epoch 4, step 235, train loss 0.485, train acc 0.836, test acc 0.828
+# epoch 5, step 235, train loss 0.473, train acc 0.841: 100%|██████████| 235/235 [00:01<00:00, 151.28it/s]
+# epoch 5, step 235, train loss 0.473, train acc 0.841, test acc 0.828
+# epoch 6, step 235, train loss 0.465, train acc 0.842: 100%|██████████| 235/235 [00:01<00:00, 156.78it/s]
+# epoch 6, step 235, train loss 0.465, train acc 0.842, test acc 0.831
+# epoch 7, step 235, train loss 0.459, train acc 0.845: 100%|██████████| 235/235 [00:01<00:00, 152.23it/s]
+# epoch 7, step 235, train loss 0.459, train acc 0.845, test acc 0.834
+# epoch 8, step 235, train loss 0.452, train acc 0.847: 100%|██████████| 235/235 [00:01<00:00, 146.69it/s]
+# epoch 8, step 235, train loss 0.452, train acc 0.847, test acc 0.832
+# epoch 9, step 235, train loss 0.447, train acc 0.848: 100%|██████████| 235/235 [00:01<00:00, 155.97it/s]
+# epoch 9, step 235, train loss 0.447, train acc 0.848, test acc 0.831
+# train loss 0.447, train acc 0.848, test acc 0.831
+# 45239.9 examples/sec
